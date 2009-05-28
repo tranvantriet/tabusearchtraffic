ab1fab4e855cac9dd0b5226f364878164b43040b
rodrigo.fraga <rodrigo.fraga@gmail.com>
2009-05-28 13:14:41 +0000

    moved calculateTourTotalDistance from class TSRouteEvaluation to class Tools. This is better

--- src/br/mackenzie/tgi/test/TSRouteEvaluation.java
+++ src/br/mackenzie/tgi/test/TSRouteEvaluation.java
@@ -1,5 +1,7 @@
 package br.mackenzie.tgi.test;
 
+import br.mackenzie.tgi.tools.Tools;
+
 /**
  * Classe responsável por validar uma solução da melhor rota Critério para
  * escolher a melhor rota: Quando o tempo que eu levei para percorrer a matriz
@@ -16,6 +18,18 @@ public class TSRouteEvaluation {
 	private int[] finalTour;
 	private double mediumVelocity = 10;
 
+	/**
+	 * Construtor público da classe TSrouteEvaluation para configurar as devidas variáveis internas.
+	 * O método não se preocupa qual roteiro é com trânsito ou sem trânsito, simplesmente 
+	 * calcula a distância e tempo total e depois avalia qual roteiro é o melhor levando em consideração
+	 * o tempo e depois a distância
+	 * 
+	 * @param correctedDistanceMatrix Matriz de distância corrigida
+	 * @param distanceMatrix Matriz de distância original
+	 * @param firstTour O primeiro roteiro
+	 * @param secondTour O secondo roteiro
+	 * @param mediumVelocity uma velocidade média especificada
+	 */
 	public TSRouteEvaluation(double[][] correctedDistanceMatrix,
 			double[][] distanceMatrix, int[] firstTour, int[] secondTour,
 			double mediumVelocity) {
@@ -24,14 +38,14 @@ public class TSRouteEvaluation {
 		this.distanceMatrix = distanceMatrix;
 		this.mediumVelocity = mediumVelocity;
 
-		double firstTourTotalTime = this.calculateTourTotalDistance(firstTour,
+		double firstTourTotalTime = Tools.calculateTourTotalDistance(firstTour,
 				correctedDistanceMatrix)/ this.mediumVelocity;
-		double firstTourTotalDistance = this.calculateTourTotalDistance(
+		double firstTourTotalDistance = Tools.calculateTourTotalDistance(
 				firstTour, distanceMatrix);
 
-		double secondTourTotalTime = this.calculateTourTotalDistance(secondTour,
+		double secondTourTotalTime = Tools.calculateTourTotalDistance(secondTour,
 				correctedDistanceMatrix) / this.mediumVelocity;
-		double secondTourTotalDistance = this.calculateTourTotalDistance(
+		double secondTourTotalDistance = Tools.calculateTourTotalDistance(
 				secondTour, distanceMatrix);
 		
 		System.out.println(String.valueOf(firstTourTotalTime).replace(".", ",") + "\t" + String.valueOf(firstTourTotalDistance).replace(".", ","));
@@ -50,22 +64,14 @@ public class TSRouteEvaluation {
 
 	}
 
+	/**
+	 * Método específico que retorna o melhor roteiro. Deve ser chamado depois de instanciado
+	 * o objeto TSRouteEvaluation
+	 * @see TSRouteEvaluation
+	 * @return um array de inteiros contendo o melhor percurso
+	 */
 	public int[] getBestTour() {
 		return this.finalTour;
-	}
-
-
-	private double calculateTourTotalDistance(int tour[],
-			double[][] localDistanceMatrix) {
-		int lastTour = 0;
-		double totalDistance = 0.0;
-
-		for (int i = 0; i < tour.length; i++) {
-			totalDistance += localDistanceMatrix[tour[i]][lastTour];
-			lastTour = tour[i];
-		}
-
-		return totalDistance;
 	}
 
 }
